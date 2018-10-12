d9a6963fcda489fb7be4d507ef2ed2a6ef37e24a
Dries Laeremans <drieslaeremans@hotmail.com>
2018-10-12 10:28:30 +0200

    Allle klassen

--- src/main/java/be/thomasmore/travelmore/domain/Reis.java
+++ src/main/java/be/thomasmore/travelmore/domain/Reis.java
@@ -1,4 +1,24 @@
 package be.thomasmore.travelmore.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Entity
+@Table(name = "reis")
 public class Reis {
+
+    @Id
+    private int id;
+    @Column(name = "naam")
+    private String naam;
+    @Column(name = "datumVertrek")
+    private Date datumVertrek;
+    @Column(name = "datumAankomst")
+    private Date datumTerug;
+    @Column(name = "prijsPerPersoon")
+    private double prijsPerPersoon;
+
 }
